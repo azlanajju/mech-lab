9d03cc4a49ca6863ddf3b1f15daed4150493cea3
azlanajju <muhammedazlan11@gmail.com>
2023-07-16 15:18:38 +0530

    added resposiveness

---
+++ style.cs
@@ -0,0 +1,56 @@
+
+@media only screen and (min-width: 980px) {
+
+
+  .sems button {
+    border: none;
+    background: none;
+    height: 40px;
+    text-decoration: none;
+    padding: 6px;
+    color: grey;
+    font-size: 35px;
+    margin: 10px;
+  }
+
+  .sub a {
+    text-decoration: none;
+    font-weight: 1000;
+    margin: 40px;
+    background: none;
+    color: white;
+    border: none;
+    font-size: 50px;
+
+  }
+
+  .subactiv a {
+    color: #6DE60D;
+
+  }
+
+  .card {
+    margin: 40px;
+    border: 1px solid #6DE60D;
+
+    color: grey;
+
+    margin-top: 20px;
+    background-color: #1c1b29;
+    border-radius: 20px;
+    box-shadow: 0 0 30px rgba(0, 0, 0, 0.18);
+  }
+
+  .container {
+    margin: 5px;
+    position: relative;
+    max-height: 60vh;
+    
+
+  }
+  .container iframe{
+    min-height: 100vh !important;
+  }
+
+
+}
